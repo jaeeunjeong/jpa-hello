504f0dba1326972919a4f372730fd5fce4469782
jaeeunjeong <jejeong000@gmail.com>
2022-11-12 16:53:33 +0900

    add : 일대다 단방향 매핑 반대편에 다대일 단방향 매핑을 읽기 전용으로 추가해서 일대다 양방향처럼 보이게 하기

--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -16,7 +16,7 @@ public class Member {
     private String username;
 
     @ManyToOne
-    @JoinColumn(name = "TEAM_ID")
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
     private Team team;
 
     @OneToOne
