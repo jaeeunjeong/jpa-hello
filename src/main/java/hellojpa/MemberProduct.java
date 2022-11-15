9eb5aa69b86534ab3385fc41b071e52811f89ee8
jaeeunjeong <jejeong000@gmail.com>
2022-11-15 23:34:42 +0900

    add : 다대다 새로운 키로 연결

---
+++ src/main/java/hellojpa/MemberProduct.java
@@ -0,0 +1,20 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+@IdClass(MemberProduct.class)
+public class MemberProduct {
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int orderAmount;
+}
---
+++ src/main/java/hellojpa/Order.java
@@ -0,0 +1,23 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Table(name = "ORDERS")
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "PRODUCT_ID")
+    private Product product;
+
+    private int orderAmount;
+}
